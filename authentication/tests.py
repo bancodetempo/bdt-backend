148eb499a2d9d1e5c5ba18d33a13e734919f4bb7
Vinícius Silva <vinicivs.monteiro@gmail.com>
2020-07-25 23:09:03 -0300

    email as variable

--- authentication/tests.py
+++ authentication/tests.py
@@ -7,10 +7,14 @@ class AuthenticationViewSetTest(APITestCase):
 
     def test_update(self):
         google_drive_spreadsheet_id = '123'
+        email = 'test@email.com'
         CustomUser.objects.create(google_drive_spreadsheet_id=google_drive_spreadsheet_id)
         user = CustomUser.objects.get(google_drive_spreadsheet_id=google_drive_spreadsheet_id)
-        response = self.client.put('/api/v0/authentication/request_user_validation/', { "email": 'test@email.com', 'google_drive_spreadsheet_id': user.google_drive_spreadsheet_id }, format="json")
+        response = self.client.put('/api/v0/authentication/request_user_validation/',
+                                   { "email": email,
+                                    'google_drive_spreadsheet_id': google_drive_spreadsheet_id },
+                                   format="json")
         user = CustomUser.objects.get(google_drive_spreadsheet_id=google_drive_spreadsheet_id)
-        self.assertEqual(user.email, 'test@email.com')
+        self.assertEqual(user.email, email)
         self.assertEqual(response.status_code, 200)
 
