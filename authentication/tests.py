2cd6299eb69edc59f9a3c2303843a1ad4628e22e
Vinícius Silva <vinicivs.monteiro@gmail.com>
2020-07-25 23:06:03 -0300

    rename spreadsheet id

--- authentication/tests.py
+++ authentication/tests.py
@@ -6,10 +6,11 @@ from authentication.models import CustomUser
 class AuthenticationViewSetTest(APITestCase):
 
     def test_update(self):
-        CustomUser.objects.create(google_drive_spreadsheet_id='123')
-        user = CustomUser.objects.get(google_drive_spreadsheet_id='123')
+        google_drive_spreadsheet_id = '123'
+        CustomUser.objects.create(google_drive_spreadsheet_id=google_drive_spreadsheet_id)
+        user = CustomUser.objects.get(google_drive_spreadsheet_id=google_drive_spreadsheet_id)
         response = self.client.put('/api/v0/authentication/request_user_validation/', { "email": 'test@email.com', 'google_drive_spreadsheet_id': user.google_drive_spreadsheet_id }, format="json")
-        user = CustomUser.objects.get(google_drive_spreadsheet_id='123')
+        user = CustomUser.objects.get(google_drive_spreadsheet_id=google_drive_spreadsheet_id)
         self.assertEqual(user.email, 'test@email.com')
         self.assertEqual(response.status_code, 200)
 
