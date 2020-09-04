a35f4d0b47a7ded12f62289c6e41a6470ddaed4b
Davi A. Wasserberg <davi.abreu@magnetis.com.br>
2020-09-04 16:00:50 -0300

    Fix in autocomplete.

--- user/tests.py
+++ user/tests.py
@@ -23,8 +23,8 @@ class UserViewSetTestCase(APITestCase):
             google_drive_spreadsheet_id="00421",
 
         )
-        first_name_searched = 'Arnold'
-        url_with_parameters = f'{self.endpoint}?search={first_name_searched}'
+        search_token = 'Arnold'
+        url_with_parameters = f'{self.endpoint}?search={search_token}'
         response = self.client.get(url_with_parameters)
 
         self.assertEquals(response.status_code, status.HTTP_200_OK)
@@ -40,14 +40,58 @@ class UserViewSetTestCase(APITestCase):
             google_drive_spreadsheet_id="00421",
 
         )
-        last_name_search_parameter = "Schwarz"
-        url_with_parameters = f'{self.endpoint}?search={last_name_search_parameter}'
+        search_token = "Schwarz"
+        url_with_parameters = f'{self.endpoint}?search={search_token}'
         response = self.client.get(url_with_parameters)
 
         self.assertEquals(response.status_code, status.HTTP_200_OK)
         response_content = response.data
         self.assertEquals(len(response_content), 1)
         self.assertEquals(response_content[0]['id'], self.user.id)
+
+    def test_search_user_by_full_name_containing(self):
+        baker.make_recipe(
+            'user.user_recipe',
+            first_name="Ricky",
+            last_name="Martin",
+            google_drive_spreadsheet_id="00421",
+
+        )
+        search_token = "Arn Schwarz"
+        url_with_parameters = f'{self.endpoint}?search={search_token}'
+        response = self.client.get(url_with_parameters)
+
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        response_content = response.data
+        self.assertEquals(len(response_content), 1)
+        self.assertEquals(response_content[0]['id'], self.user.id)
+
+    def test_search_user_by_first_name_containing_and_part_of_last_name_containing(self):
+        search_token = "Arn Schw"
+        url_with_parameters = f'{self.endpoint}?search={search_token}'
+        response = self.client.get(url_with_parameters)
+
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        response_content = response.data
+        self.assertEquals(len(response_content), 1)
+        self.assertEquals(response_content[0]['id'], self.user.id)
+
+    def test_search_user_by_last_name_big_name(self):
+        user_big_name = baker.make_recipe(
+            'user.user_recipe',
+            first_name="Ricardo",
+            last_name="Monteiro e Lima",
+            google_drive_spreadsheet_id="00421",
+
+        )
+        search_token = "Lima"
+        url_with_parameters = f'{self.endpoint}?search={search_token}'
+        response = self.client.get(url_with_parameters)
+
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        response_content = response.data
+        self.assertEquals(len(response_content), 1)
+        self.assertEquals(response_content[0]['id'], user_big_name.id)
 
     def test_search_user_by_google_drive_exact_value(self):
         baker.make_recipe(
@@ -57,8 +101,8 @@ class UserViewSetTestCase(APITestCase):
             google_drive_spreadsheet_id="00421",
 
         )
-        google_drive_spreadsheet_id_search_value = "00425"
-        url_with_parameters = f'{self.endpoint}?search={google_drive_spreadsheet_id_search_value}'
+        search_token = "00425"
+        url_with_parameters = f'{self.endpoint}?search={search_token}'
         response = self.client.get(url_with_parameters)
 
         self.assertEquals(response.status_code, status.HTTP_200_OK)
@@ -78,8 +122,8 @@ class UserViewSetTestCase(APITestCase):
             google_drive_spreadsheet_id="00421",
 
         )
-        google_drive_spreadsheet_id_search_value = "0042"
-        url_with_parameters = f'{self.endpoint}?search={google_drive_spreadsheet_id_search_value}'
+        search_token = "0042"
+        url_with_parameters = f'{self.endpoint}?search={search_token}'
 
         response = self.client.get(url_with_parameters)
         self.assertEquals(response.status_code, status.HTTP_200_OK)
