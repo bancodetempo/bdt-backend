6967c6c88a6ae0265a1735761d73a65402dc9def
daviaws <davi.abreu.w@gmail.com>
2020-09-04 19:00:53 -0300

    User search query logic fix (#6)

--- user/filters.py
+++ user/filters.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 from django.db.models import Q
 from django.db.models.query import QuerySet
 from django_filters import rest_framework as filters
@@ -9,14 +11,16 @@ class UserFilter(filters.FilterSet):
     search = filters.CharFilter(field_name="search", method="filter_search")
 
     def filter_search(self, queryset: QuerySet, name: str, value: str) -> QuerySet:
-        user_by_first_name = Q(first_name__icontains=value)
-        user_by_last_name = Q(last_name__icontains=value)
+        name, last_name, *_ = chain(value.split(" ", 1), [""])
+
+        user_by_full_name = Q(first_name__icontains=name) & Q(last_name__icontains=last_name)
+        user_by_last_name = Q(last_name__icontains=name)
         user_by_google_drive_spreadsheet_id = Q(
             google_drive_spreadsheet_id=value
         )
 
         result_queryset = queryset.filter(
-            user_by_first_name | user_by_last_name | user_by_google_drive_spreadsheet_id
+            user_by_full_name | user_by_last_name | user_by_google_drive_spreadsheet_id
         )
 
         return result_queryset
