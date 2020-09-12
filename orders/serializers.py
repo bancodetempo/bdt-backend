cd2e0c9cc44b3aac1ea8524d081f3454012a2797
Ricardo M. Lima <ricardolima89@gmail.com>
2020-09-12 12:50:10 -0300

    Correct Order validation with tests

--- orders/serializers.py
+++ orders/serializers.py
@@ -3,8 +3,18 @@ from .models import Order
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    status = serializers.IntegerField(required=False)
 
     class Meta:
         model = Order
-        fields = '__all__'
+        fields = ('requester', 'grantor', 'order_price', 'description')
+
+    def validate(self, attrs):
+        requester = attrs['requester']
+        order_price = attrs['order_price']
+
+        requester_account_balance = requester.account.balance
+
+        if requester_account_balance < order_price:
+            raise serializers.ValidationError('Saldo insuficiente')
+
+        return attrs
